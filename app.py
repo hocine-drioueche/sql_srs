05fdf3bbe13b9f6435f0fa343822206b590e5f4a
hocine-drioueche <drioueche.hocine@gmail.com>
2024-10-30 17:55:01 +0100

    ajout d'une database avec les exercices et un memory state

--- app.py
+++ app.py
@@ -24,48 +24,54 @@ answer_str = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution_df = duckdb.sql(answer_str).df()
+#solution_df = duckdb.sql(answer_str).df()
 
+
+con=  duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review ?",
-        ("Joins", "GroupBy", "Windows functions"),
+        ("cross_joins", "GroupBy", "Windows functions"),
         index=None,
         placeholder="Select a thene ...",
     )
-    st.write("You selected:", option)
+    st.write("You selected:", theme)
+
+
+    exercise=con.execute(f"SELECT * FROM memory_state WHERE theme='{theme}' ").df()
+    st.write(exercise)
 
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
+#
+# if query:
+#     result = duckdb.sql(query).df()
+#     st.dataframe(result)
+#
+#     try:
+#         result = result[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("Some columns are missing")
+#
+#     n_lines_difference = result.shape[0] - solution_df.shape[0]
+#     if n_lines_difference != 0:
+#         st.write(
+#             f"result has a {n_lines_difference} lines difference with the solution"
+#         )
+#
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+#
+# with tab2:
+#     st.write("table : beverages")
+#     st.dataframe(beverages)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("expected:")
+#     st.dataframe(solution_df)
 
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
-
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"result has a {n_lines_difference} lines difference with the solution"
-        )
-
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table : beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution_df)
-
-with tab3:
-    st.write(answer_str)
+# with tab3:
+#     st.write(answer_str)
