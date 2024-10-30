bacdfc12ecaf35a90ff207f01ea5929baf6fb024
hocine-drioueche <drioueche.hocine@gmail.com>
2024-10-30 20:16:21 +0100

    use usr input to display exercise specifics

--- app.py
+++ app.py
@@ -3,29 +3,7 @@ import io
 import duckdb
 import pandas as pd
 import streamlit as st
-
-csv = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(csv))
-
-csv2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-food_items = pd.read_csv(io.StringIO(csv2))
-
-answer_str = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-#solution_df = duckdb.sql(answer_str).df()
-
+import ast
 
 con=  duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -46,9 +24,9 @@ with st.sidebar:
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 #
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
 #
 #     try:
 #         result = result[solution_df.columns]
@@ -63,15 +41,17 @@ query = st.text_area(label="votre code SQL ici", key="user_input")
 #         )
 #
 #
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 #
-# with tab2:
-#     st.write("table : beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#     st.dataframe(solution_df)
+with tab2:
+    exercise_tables= ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table : {table}")
+        df_table =con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
 
-# with tab3:
-#     st.write(answer_str)
+with tab3:
+    exercise_name= exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql","r") as f:
+        answer=f.read()
+    st.write(answer)
